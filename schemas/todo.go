074ae57229178451a1883501eec7a07b2f02d2d6
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-08-11 12:52:13 -0300

    feat: add todo schema package

---
+++ schemas/todo.go
@@ -0,0 +1,10 @@
+package schemas
+
+import ("gorm.io/gorm")
+
+type Todo struct {
+	gorm.Model
+	Title string `json:"title"`
+	Description string `json:"description"`
+	Completed bool `json:"completed"`
+}
