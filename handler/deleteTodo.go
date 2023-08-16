c9434ce7af9b28be79edd6e6904528677672af2e
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-08-16 18:53:53 -0300

    feat: add delete todo handler logic

--- handler/deleteTodo.go
+++ handler/deleteTodo.go
@@ -1,14 +1,29 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tiburciohugo/rest-api-test/schemas"
 )
 
 func DeleteTodoHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Delete TodoHandler",
-	})
+	id := c.Query("id")
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	todo := schemas.Todo{}
 
+	if err := db.First(&todo, id).Error; err != nil {
+		sendError(c, http.StatusNotFound, fmt.Sprintf("todo with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&todo).Error; err != nil {
+		sendError(c, http.StatusInternalServerError, fmt.Sprintf("error deleting todo with id: %s", id))
+		return
+	}
+	sendSuccess(c, "todo deleted successfully", todo)
 }
