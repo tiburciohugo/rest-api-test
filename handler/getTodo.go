99dcd5547722d5d1a7dfa1a9081f71f8e695bc70
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-08-16 18:54:33 -0300

    feat: add show todo handler logic

--- handler/getTodo.go
+++ handler/getTodo.go
@@ -1,14 +1,24 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tiburciohugo/rest-api-test/schemas"
 )
 
 func ShowTodoHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "GET TodoHandler",
-	})
+	id := c.Query("id")
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	todo := schemas.Todo{}
 
+	if err := db.First(&todo, id).Error; err != nil {
+		sendError(c, http.StatusNotFound, fmt.Sprintf("todo with id: %s not found", id))
+		return
+	}
+	sendSuccess(c, "todo found successfully", todo)
 }
